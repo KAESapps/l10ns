b9adc29b9b19f49f38e1f335c8b5c15b6964cc7f
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2016-07-28 22:15:49 +0200

    git commit and push after compile

--- interface/contents/LeftMenu/LeftMenuApi.js
+++ interface/contents/LeftMenu/LeftMenuApi.js
@@ -1,15 +1,43 @@
 
-var cli = require('../../../libraries');
+// var cli = require('../../../libraries');
+var execa = require('execa')
 
 module.exports = function(app) {
   app.get('/api/compile', function(request, response) {
-    cli.initialize()
+    // cli.initialize()
+    Promise.resolve()
       .then(function() {
-        cli.compile();
+        // cli.compile();
+        return execa.shell('l10ns compile')
+      })
+      .then(function() {
+        return execa.shell('git commit -a -m "localizations"')
+      })
+      .then(function() {
+        return execa.shell('git push')
+      })
+      .then(function() {
+        response.sendStatus(200);
+      })
+      .catch(function(error) {
+        console.error(error)
+        response.status(500).send(error.stack);
+      });
+  });
+  app.get('/api/update', function(request, response) {
+    Promise.resolve()
+      .then(function() {
+        return execa.shell('git pull')
+      })
+      .then(function() {
+        return execa.shell('l10ns update')
+      })
+      .then(function() {
         response.send('ok');
       })
-      .fail(function(error) {
-        response.send(500);
+      .catch(function(error) {
+        console.error(error)
+        response.status(500).send(error.stack);
       });
   });
 }
